bfe703a7040fb6cc52818d4961c421afea2ec72f
alicjak1519 <akrzeminska1519@gmail.com>
2024-11-05 22:50:49 +0100

    Add boxplot logging to wnb

--- plot_results.py
+++ plot_results.py
@@ -1,49 +1,51 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import os
 
+
+def plot_results(subfolder_path: str, return_fig: bool = False):
+    csv_path = os.path.join(subfolder_path, 'results.csv')
+
+    if os.path.exists(csv_path):
+        try:
+            data = pd.read_csv(csv_path, delimiter=';')
+        except Exception as e:
+            print(f"An error occurred while loading '{csv_path}': {e}")
+            return
+
+        # Ensure that the relevant columns exist
+        if not {'LABEL', 'DICE'}.issubset(data.columns):
+            print(
+                f"Error: The required columns 'LABEL' and 'DICE' do not exist in the data for '{csv_path}'.")
+            return
+
+        # Group the data by label and extract the Dice coefficients
+        labels = ["WhiteMatter", "GreyMatter", "Hippocampus", "Amygdala", "Thalamus"]
+        label_data = [data[data['LABEL'] == label]['DICE'].dropna() for label in labels]
+
+        # Plot the Dice coefficients per label in a boxplot
+        fig = plt.figure()
+        plt.boxplot(label_data, labels=labels)
+        plt.xlabel('Labels')
+        plt.ylabel('Dice Coefficients')
+        plt.title(f'Boxplot of Dice Coefficients per Label for {subfolder}')
+
+        if return_fig:
+            return fig
+        else:
+            plt.show()
+
+
 def main():
-    # todo: load the "results.csv" file from the mia-results directory
-    # todo: read the data into a list
-    # todo: plot the Dice coefficients per label (i.e. white matter, gray matter, hippocampus, amygdala, thalamus)
-    #  in a boxplot
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    mia_result_dir = os.path.join(script_dir, 'mia-result')
 
     try:
-
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        mia_result_dir = os.path.join(script_dir, 'mia-result')
-
         # Iterate through each subfolder in the mia-result directory
         for subfolder in os.listdir(mia_result_dir):
             subfolder_path = os.path.join(mia_result_dir, subfolder)
-            if os.path.isdir(subfolder_path):
-                csv_path = os.path.join(subfolder_path, 'results.csv')
+            plot_results(subfolder_path)
 
-
-                if os.path.exists(csv_path):
-                    try:
-                        data = pd.read_csv(csv_path, delimiter=';')
-                    except Exception as e:
-                        print(f"An error occurred while loading '{csv_path}': {e}")
-                        continue
-
-                    # Ensure that the relevant columns exist
-                    if not {'LABEL', 'DICE'}.issubset(data.columns):
-                        print(f"Error: The required columns 'LABEL' and 'DICE' do not exist in the data for '{csv_path}'.")
-                        continue
-
-                    # Group the data by label and extract the Dice coefficients
-                    labels = ["WhiteMatter", "GreyMatter", "Hippocampus", "Amygdala", "Thalamus"]
-                    label_data = [data[data['LABEL'] == label]['DICE'].dropna() for label in labels]
-
-                    # Plot the Dice coefficients per label in a boxplot
-                    plt.figure()
-                    plt.boxplot(label_data, labels=labels)
-                    plt.xlabel('Labels')
-                    plt.ylabel('Dice Coefficients')
-                    plt.title(f'Boxplot of Dice Coefficients per Label for {subfolder}')
-                    plt.show()
     except FileNotFoundError:
         print("Error: The 'mia-result' directory could not be found.")
         return
--- wnb_logging.py
+++ wnb_logging.py
@@ -1,8 +1,10 @@
 import wandb
 import pandas as pd
 
+from plot_results import plot_results
 
-def log_metric_in_wab(model, result_summary_file, pre_process_params):
+
+def log_metric_in_wab(model, result_summary_file, pre_process_params, subfolder_path):
     wandb.init(project="mialab", config=model.get_params())
 
     results_df = pd.read_csv(result_summary_file, sep=";")
@@ -17,5 +19,8 @@ def log_metric_in_wab(model, result_summary_file, pre_process_params):
 
     for param in pre_process_params.keys():
         wandb.summary[param] = pre_process_params[param]
+
+    results_fig = plot_results(subfolder_path, return_fig=True)
+    wandb.log({"plot": wandb.Image(results_fig)})
 
     wandb.finish()
