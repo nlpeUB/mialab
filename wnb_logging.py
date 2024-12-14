bd0d240376b55dcb1579166ef4dd5f469034e3fa
alicjak1519 <akrzeminska1519@gmail.com>
2024-12-14 10:32:03 +0100

    Update results analysis

--- wnb_logging.py
+++ wnb_logging.py
@@ -15,6 +15,7 @@ FEATURE_NAMES = [
     'edge_feature'
 ]
 
+
 def log_metric_in_wnb(model, feature_names, result_summary_file, pre_process_params, subfolder_path):
     wandb.init(project="mialab", config=model.get_params())
 
@@ -22,8 +23,10 @@ def log_metric_in_wnb(model, feature_names, result_summary_file, pre_process_par
     results_df["KEY"] = results_df.apply(lambda row: f'{row["STATISTIC"]}_{row["METRIC"]}_{row["LABEL"]}', axis=1)
     dice_dict = results_df[["KEY", "VALUE"]].set_index("KEY")["VALUE"].to_dict()
 
-    dice_dict["MEAN_DICE"] = results_df[(results_df["STATISTIC"] == "MEAN") & (results_df["METRIC"] == "DICE")]["VALUE"].mean()
-    dice_dict["MEAN_HDRFDST"] = results_df[(results_df["STATISTIC"] == "MEAN") & (results_df["METRIC"] == "HDRFDST")]["VALUE"].mean()
+    dice_dict["MEAN_DICE"] = results_df[(results_df["STATISTIC"] == "MEAN") & (results_df["METRIC"] == "DICE")][
+        "VALUE"].mean()
+    dice_dict["MEAN_HDRFDST"] = results_df[(results_df["STATISTIC"] == "MEAN") & (results_df["METRIC"] == "HDRFDST")][
+        "VALUE"].mean()
 
     for metric in dice_dict.keys():
         wandb.summary[metric] = dice_dict[metric]
@@ -39,19 +42,21 @@ def log_metric_in_wnb(model, feature_names, result_summary_file, pre_process_par
 
     wandb.finish()
 
+
 def get_run_by_name(api, run_name, entity_name, project_name):
     runs = api.runs(f"{entity_name}/{project_name}")
-    
+
     run_id = None
     for run in runs:
         if run.name == run_name:
             return run
 
+
 def get_runs_between_the_timestamps(api, first_valid_run_timestamp, last_valid_run_timestamp, entity, project):
     runs = api.runs(f"{entity}/{project}")
-    
+
     run_dict_list = []
-    
+
     for run in runs:
         if "_timestamp" not in run.summary.keys():
             continue
@@ -59,16 +64,17 @@ def get_runs_between_the_timestamps(api, first_valid_run_timestamp, last_valid_r
         if (timestamp >= first_valid_run_timestamp) and (timestamp <= last_valid_run_timestamp):
             summary = run.summary._json_dict
             config = {k: v for k, v in run.config.items() if not k.startswith("_")}
-            
+
             run_dict_list.append({
                 "run_id": run.id,
                 "run_name": run.name,
                 **summary,
                 **config
             })
-            
+
     runs_df = pd.DataFrame(run_dict_list)
     return runs_df
+
 
 def count_features(row):
     features_count = 0
@@ -79,13 +85,14 @@ def count_features(row):
         features_count *= 2
     return features_count
 
+
 def download_df_between_the_timestamps(first_valid_run_name, last_valid_run_name):
     api = wandb.Api()
     entity, project = "alicja-krzeminska-sciga-dl", "mialab"
 
     first_valid_run = get_run_by_name(api, first_valid_run_name, entity, project)
     first_valid_run_timestamp = first_valid_run.summary["_timestamp"]
-    
+
     last_valid_run = get_run_by_name(api, last_valid_run_name, entity, project)
     last_valid_run_timestamp = last_valid_run.summary["_timestamp"]
 
@@ -93,7 +100,7 @@ def download_df_between_the_timestamps(first_valid_run_name, last_valid_run_name
 
     metric_columns = [c for c in runs_df.columns if "_DICE" in c or "_HDRFDST" in c]
     interesting_columns = metric_columns + FEATURE_NAMES + ["n_estimators", "texture_window", "_timestamp"]
-    
+
     runs_df = runs_df.sort_values(by="_timestamp", ascending=False)
     runs_df = runs_df[interesting_columns]
 
